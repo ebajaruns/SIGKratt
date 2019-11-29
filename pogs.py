0a8fc9f862242743b99ff535e06aead6121a40b0
trenerise <39572451+trenerise@users.noreply.github.com>
2019-11-29 11:51:21 +0200

    updates

--- pogs.py
+++ pogs.py
@@ -1,26 +1,15 @@
 # -- coding: utf-8 -
 
-import os
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
   
 @app.route('/')
 def index():
-    os.system("mpg321 hello.mp3") 
-    if request.method == 'POST':
-        if request.form['submit_button'] == 'Do Something':
-            print("poga nospiesta")
-            os.system("mpg321 hello.mp3")         
-        elif request.form['submit_button'] == 'Do Something Else':
-            os.system("mpg321 service.mp3") 
-        else:
-            return render_template('index.html')
-    elif request.method == 'GET':
-        print("No Post Back Call")
-    return render_template('index.html')
     
+    return render_template('index.html')
     
 @app.route('/uzn')
 def cakes():
